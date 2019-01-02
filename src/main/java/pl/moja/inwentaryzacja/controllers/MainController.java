0d5e696ece09957546fba65d9d1361ac859df085
kohrd <konrad.janeczek@protonmail.com>
2019-01-02 16:04:04 +0100

    lekcja 27

--- src/main/java/pl/moja/inwentaryzacja/controllers/MainController.java
+++ src/main/java/pl/moja/inwentaryzacja/controllers/MainController.java
@@ -1,7 +1,13 @@
 package pl.moja.inwentaryzacja.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class MainController {
 
@@ -9,12 +15,31 @@ public class MainController {
     private TopMenuButtonsController topMenuButtonsController; // wstrzykniecie kontrollera topMenuButtons
 
     @FXML
-    private BorderPane borderPane;
+    private BorderPane borderPane; // główny borderPane formtaki main , do niego wkładamy inne formatki
 
     @FXML
-    private void initialize(){
+    private void initialize() {
         topMenuButtonsController.setMainController(this); // przekazujemy referencje do topMenuButtonsController
 
 
     }
+
+
+    public void setCenter(String fxmlPath) {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource(fxmlPath));
+
+        Locale.setDefault(new Locale("en")); // ustawianei jezyka dla bundless na odpowiedni
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
+
+        Parent parent = null;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        borderPane.setCenter(parent);
+
+    }
+
 }
--- src/main/java/pl/moja/inwentaryzacja/controllers/TopMenuButtonsController.java
+++ src/main/java/pl/moja/inwentaryzacja/controllers/TopMenuButtonsController.java
@@ -2,27 +2,50 @@ package pl.moja.inwentaryzacja.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 
 public class TopMenuButtonsController {
 
+    public static final String FXML_LIBRARY_FXML = "/fxml/Library.fxml";
+    public static final String FXML_LIST_BOOKS_FXML = "/fxml/ListBooks.fxml";
+    public static final String FXML_STATISTICS_FXML = "/fxml/Statistics.fxml";
+
+
     private MainController mainController; // referencja do maincontrollera tak zeby topMenuButtons mógł wpływac na mainBorderPane
 
+    @FXML
+    private ToggleGroup toggleButtons;
 
 
     @FXML
-    public void openLibrary(){
-        System.out.println("open library");
-
-    }
-    @FXML
-    public void openListBooks(){
-        System.out.println("openListBooks");
+    public void openLibrary() {
+        mainController.setCenter(FXML_LIBRARY_FXML);
 
     }
 
     @FXML
-    public void openStatistics(){
-        System.out.println("openStatiffstics");
+    public void openListBooks() {
+        mainController.setCenter(FXML_LIST_BOOKS_FXML);
+
+    }
+
+    @FXML
+    public void openStatistics() {
+
+        mainController.setCenter(FXML_STATISTICS_FXML);
+
+    }
+
+    @FXML
+    public void addBook() {
+        // opdznaczanie grupy toggle button jesl wybierzemy przcisk addBook
+        // nie jets null tzn jest jakis przycisk z toggleGroup zaznaczony
+        // najpierw musimy sprawdzic czy jest cos zaznaczoen zeby nie poleciał nullpointerexc
+        if (toggleButtons.getSelectedToggle() != null) {
+            toggleButtons.getSelectedToggle().setSelected(false);
+        }
+        mainController.setCenter("/fxml/AddBook.fxml");
+
 
     }
 
