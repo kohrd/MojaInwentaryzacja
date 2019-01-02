ac3f6e7c231864e9a715f02b33f298de9ac8c463
kohrd <konrad.janeczek@protonmail.com>
2019-01-02 14:46:09 +0100

    lekcja 26

--- src/main/java/pl/moja/inwentaryzacja/Main.java
+++ src/main/java/pl/moja/inwentaryzacja/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
 
@@ -16,10 +19,15 @@ public class Main extends Application {
 
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml"));
+
+        Locale.setDefault(new Locale("en")); // ustawianei jezyka dla bundless na odpowiedni
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
+
         BorderPane borderPane = loader.load();
         Scene scene = new Scene(borderPane);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("inwentaryzacjaaaaaaaacccccc");
+        primaryStage.setTitle(bundle.getString("title.application")); //ustawianie tytutłu aplikacji za pmocą bundles
         primaryStage.show();
 
     }
