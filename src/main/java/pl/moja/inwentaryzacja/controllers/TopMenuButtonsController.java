922d47e341f6c47bfc516a33d3ac571f7e9fb82a
kohrd <konrad.janeczek@protonmail.com>
2019-01-02 12:35:53 +0100

    5th commit

---
+++ src/main/java/pl/moja/inwentaryzacja/controllers/TopMenuButtonsController.java
@@ -0,0 +1,26 @@
+package pl.moja.inwentaryzacja.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class TopMenuButtonsController {
+
+    @FXML
+    public void openLibrary(){
+        System.out.println("open library");
+
+    }
+    @FXML
+    public void openListBooks(){
+        System.out.println("openListBooks");
+
+    }
+
+    @FXML
+    public void openStatistics(){
+        System.out.println("openStatistics");
+
+    }
+
+
+}
