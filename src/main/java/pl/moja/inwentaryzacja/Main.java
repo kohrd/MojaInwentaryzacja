42fc572b6445266e2bf1cb60bd56f8c5dc2fc87c
kohrd <konrad.janeczek@protonmail.com>
2019-01-02 11:28:14 +0100

    1st try

---
+++ src/main/java/pl/moja/inwentaryzacja/Main.java
@@ -0,0 +1,26 @@
+package pl.moja.inwentaryzacja;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml"));
+        BorderPane borderPane = loader.load();
+        Scene scene = new Scene(borderPane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("inwentaryzacja");
+        primaryStage.show();
+
+    }
+}
---
+++ src/main/java/pl/moja/inwentaryzacja/controllers/MainController.java
@@ -0,0 +1,6 @@
+package pl.moja.inwentaryzacja.controllers;
+
+public class MainController {
+
+
+}
